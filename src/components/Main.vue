51660ac4f566e57cb22c48a591abc7516ab4346f
Jack-rm <jackcnsr94@gmail.com>
2021-10-07 12:48:31 +0200

    Main sections inner structure completed

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -158,12 +158,116 @@
 
     </section>
 
-    <section>
+    <section class="my_customer-info pb-2">
+      <div class="container">
+        <div class="row">
+          <div class="col-12">
+            <div class="d-flex justify-content-between">
+              <div class="flex-grow-1 text-center">
+                <h5>Testimonial</h5>
+              </div>
+              <div class="my_decoration-line">
+                <p>linea</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-12">
+            <h3 class="text-end"> What's the customer say about our work</h3>
+          </div>
+        </div>
+      </div>
+    </section>
 
-      <div>
-        
+    <section class="my_customer-section mb-5">
+      <div class="container">
+
+        <div class="row">
+          <div class="col-4">
+            <div>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus et vitae rem voluptates ipsum veritatis magni assumenda eum tempore aspernatur corporis dicta, ea fugit, fugiat sit ullam consequatur tenetur? Culpa.</p>
+              <h4>Vera Duncan</h4>
+              <p>Amazon inc.</p>
+            </div>
+          </div>
+          <div class="col-4">
+            <div>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus et vitae rem voluptates ipsum veritatis magni assumenda eum tempore aspernatur corporis dicta, ea fugit, fugiat sit ullam consequatur tenetur? Culpa.</p>
+              <h4>Vera Duncan</h4>
+              <p>Amazon inc.</p>
+            </div>
+          </div>
+          <div class="col-4">
+            <div>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus et vitae rem voluptates ipsum veritatis magni assumenda eum tempore aspernatur corporis dicta, ea fugit, fugiat sit ullam consequatur tenetur? Culpa.</p>
+              <h4>Vera Duncan</h4>
+              <p>Amazon inc.</p>
+            </div>
+          </div>
+        </div>
+      
+      </div>
+    </section>
+      
+    <section class="my_news-section pt-5 pb-5">
+      <div class="container">
+        <div class="row">
+          <div class="col-12">
+            <div class="d-flex align-items-center">
+              <div class="my_decoration-line"></div>
+              <h6>Blog</h6>
+            </div>
+          </div>
+          <div class="col-12">
+            <div class="d-flex justify-content-between align-items-center">
+              <h3>The recent news you must read it</h3>
+              <button>view all</button>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-6">
+            <div class="my_news-box">
+              <div class="d-flex flex-column justify-content-evenly">
+                <div class="d-flex">
+                  <p>July 4, 2019 </p>
+                  <span>by paul</span>
+                </div>
+                <h6>Canadian Consulting Film acquired by UK Giant</h6>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis deleniti ratione minima ut dolorum, nulla voluptatem sapiente corrupti autem nostrum molestiae, ad praesentium esse obcaecati minus atque, at magnam quidem?</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-6">
+            <div class="my_news-box">
+              <div class="d-flex flex-column justify-content-evenly">
+                <div class="d-flex">
+                  <p>July 4, 2019 </p>
+                  <span>by paul</span>
+                </div>
+                <h6>Canadian Consulting Film acquired by UK Giant</h6>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis deleniti ratione minima ut dolorum, nulla voluptatem sapiente corrupti autem nostrum molestiae, ad praesentium esse obcaecati minus atque, at magnam quidem?</p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
 
+    </section>
+
+    <section class="my_work-section mt-5 p-5">
+      <div class="container">
+        <div class="row">
+          <div class="col-12 text-center">
+            <h2>let's talk about work</h2>
+          </div>
+          <div class="col-12 text-center">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis iste numquam nisi quaerat tenetur aperiam, nihil vero animi quo labore eos explicabo velit, voluptate dolore nesciunt facere consequuntur commodi tempora?</p>
+          </div>
+          <div class="col-12 text-center">
+            <button>Get Started</button>
+          </div>
+        </div>
+      </div>
     </section>
 
   </div>
@@ -253,5 +357,8 @@ export default {
   padding: 100px 0px 50px 0px;
 }
 
+.my_customer-section{
+  background: linear-gradient(to left, white 85%, transparent 0);
+}
 
 </style>
